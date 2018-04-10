c1beff63ebbc093ab8c9fd030d0381b17caa8d7d
Alexander Soklev <alexander.soklev@chaosgroup.com>
2018-04-10 10:34:26 +0300

    Renamed the timer elapsedMilliseconds to elapsedSeconds as the result it gives is in fractions of seconds.
    Made the start method update the end point as well. This resolves issues where due to missing call to stop()
    elapsed time could return a negative value

--- Timer.h
+++ Timer.h
@@ -6,22 +6,25 @@ namespace a7az0th {
 
 class Timer {
 public:
-	Timer() { startPoint = endPoint = std::chrono::system_clock::now();};
+	Timer() { init(); }
 	~Timer() {};
 
-	void start() { startPoint = std::chrono::system_clock::now(); }
+	void start() { init(); }
 	void stop() { endPoint = std::chrono::system_clock::now(); }
 
-	/// Returns the elapsed time in seconds
-	float elapsedMilliseconds() {
-		std::chrono::milliseconds total_ms = std::chrono::duration_cast<std::chrono::milliseconds>(endPoint - startPoint);
+	// Returns the elapsed time in seconds
+	float elapsedSeconds() {
+		auto& total_ms = std::chrono::duration_cast<std::chrono::milliseconds>(endPoint - startPoint);
 		return total_ms.count() * 0.001f;
 	}
 
 private:
-	/// Disallow evil contructors
-	Timer(const Timer&);
-	Timer& operator=(const Timer&);
+	void init() {
+		startPoint = endPoint = std::chrono::system_clock::now();
+	}
+	// Disallow evil contructors
+	Timer(const Timer&) = delete;
+	Timer& operator=(const Timer&) = delete;
 
 	std::chrono::system_clock::time_point startPoint;
 	std::chrono::system_clock::time_point endPoint;
